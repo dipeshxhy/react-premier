a8d6645730fd9f99b30155c8c66fe9081730ae40
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-13 00:01:58 +0545

    feat(marquee): add marquee text

---
+++ src/components/TextMarquee.jsx
@@ -0,0 +1,23 @@
+
+const TextMarquee = () => {
+  return (
+        <div className="mt-20 border-t-b overflow-hidden">
+          <div className="flex flex-nowrap  gap-6 marquee-text text-7xl h-[10vh] items-center  ">
+            <h1 className="shrink-0 uppercase ">Premier Model 2020</h1>
+            <h1 className="shrink-0   uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0   uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+            <h1 className="shrink-0  uppercase">Premier Model 2020</h1>
+          </div>
+    </div>
+  )
+}
+
+export default TextMarquee
